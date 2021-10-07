de514380b0e2c42cdb2bd019715e8a96c11ed99b
tallsilverbirch <1091379711@qq.com>
2021-10-07 16:50:21 +0800

    Add files via upload

---
+++ my_math.py
@@ -0,0 +1,15 @@
+def square(x):
+    '''
+    make it try
+
+    >>> square(2)
+    4
+    >>> square(3)
+    9
+    '''
+    return x*x
+
+if __name__=='__main__':
+    import doctest, my_math
+    doctest.testmod(my_math)
+   testmode() =a,b,c
