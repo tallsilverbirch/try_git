b0fd391b6c3fdadbaa7a49bee2f80682386dad25
tallsilverbirch <zhangbaihua_neu@qq.com>
2023-03-23 14:57:31 +0800

    ningbo try

--- add.py
+++ add.py
@@ -1,9 +1,12 @@
-snumber=100
+snumbersj100
 print(number)
 number1=200
-print(number1）
 number=2
 print(number2)
+print(number1）
+print(number1）
+print(number1）
+print(number1）
 number3=200
 print(number3)
 print('what a fucking day")
